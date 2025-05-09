c21ded558aecb03fbc76659f2795d509c1913dd6
fillip husebø <felipehusebo@gmail.com>
2025-05-09 11:16:33 +0200

    created a fetch hook

--- src/routes/home/featured.jsx
+++ src/routes/home/featured.jsx
@@ -1,7 +1,26 @@
+import {useFetch} from "../../hooks/api/useFetch.jsx";
+
 export function Featured(){
-    return(
+    const {data: venues, loading, error} = useFetch("https://v2.api.noroff.dev/holidaze/venues");
+
+    if(loading){
+        return(
+            <>Loading...</>
+        )
+    }
+    if(error){
+        console.log(error)
+    }
+
+    if (!venues || venues.length === 0) {
+        return <>No venues available.</>;
+    }
+
+    return (
         <>
-            home:)
+            {venues.map((venue) => (
+                <h1 key={venue.id}>{venue.name}</h1>
+            ))}
         </>
-    )
+    );
 }
