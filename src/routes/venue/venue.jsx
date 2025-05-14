814c7404292830189dc199ecd682ba54a11040b1
fillip husebø <felipehusebo@gmail.com>
2025-05-14 18:43:11 +0200

    fixed tailwind issue

--- src/routes/venue/venue.jsx
+++ src/routes/venue/venue.jsx
@@ -1,7 +1,26 @@
-export function Venue() {
-    return(
+import {useFetch} from "../../hooks/api/useFetch.jsx";
+import {Link, useParams} from "react-router-dom";
+import {API_VENUE} from "../../utilities/constants.js";
+
+export function Venue(){
+    const {id} = useParams(id)
+    const {data: venues, loading, error} = useFetch(`${API_VENUE}/id`);
+
+    if(loading){
+        return(
+            <>Loading...</>
+        )
+    }
+    if(error){
+        console.log(error)
+    }
+
+    if (!venues || venues.length === 0) {
+        return <>No venues available.</>;
+    }
+
+    return (
         <>
-            venue page
         </>
-    )
+    );
 }
